b1bea88799c342c89450693ed81e25a0565b7c9e
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2022-09-02 11:56:33 +0200

    update

--- Sharing_big_document_by_email/13_email_approved_sender.tf
+++ Sharing_big_document_by_email/13_email_approved_sender.tf
@@ -1,6 +1,5 @@
 resource oci_email_sender sharing {
-  provider       = oci.ashburn
-  compartment_id = var.reg_compartment_id
+  compartment_id = var.compartment_id
   email_address  = "noreply@oci-sharing-function.com"
 }
 
